f1107ac3b3bb1bd6e335e223a97ed9fe53f97c71
Benjamin Lorieul <benjamin.lorieul.pro@gmail.com>
2024-12-18 17:48:37 +0100

    evolution languages

--- Projets/Portfolio/index.php
+++ Projets/Portfolio/index.php
@@ -20,7 +20,7 @@
         <!-- MAIN -->
         <div class="main-acceuil">
             <h2>Salut!👋<br>Je suis Benjamin</h2>
-            <p class="main-status">Apprenti développeur web ISCB</p>
+            <p class="main-status">Apprenti développeur web</p>
             <div class="status">
                 <p>👨‍🎓 Apprenti</p>
                 <p>&lt;/&gt; développeur</p>
@@ -36,10 +36,10 @@
             </div>
             <div class="presentation">
                 <h3>Petite Présentation</h3>
-                <p>Je m'apelle <span class="presentation-important">Benjamin Lorieul</span>, jeune apprenti développeur web de 18ans. <br></p>
+                <p>Je m'apelle <span class="important">Benjamin Lorieul</span>, jeune apprenti développeur web de 18ans. <br></p>
                 <p>J'adore coder des projets, construire des applications ou créer des algorithmes pour résoudre divers problèmes.</p>
-                <p>Je suis principalement axé sur la création de <span class="presentation-important">sites web</span> ainsi que de <span class="presentation-important">solutions logicielles et applications</span>.</p>
-                <p>J'ai appris majoritairement en auto-didacte avec <span class="presentation-important">HTML5, CSS et PHP</span> et je suis aujourd'hui étudiant en 1ère année de <span class="presentation-important">BTS SIO SLAM</span> dans la ville de Tours.</p>
+                <p>Je suis principalement axé sur la création de <span class="important">sites web</span> ainsi que de <span class="important">solutions logicielles et applications</span>.</p>
+                <p>J'ai appris majoritairement en auto-didacte avec <span class="important">HTML5, CSS et PHP</span> et je suis aujourd'hui étudiant en 1ère année de <span class="important">BTS SIO SLAM</span> dans la ville de Tours.</p>
             </div>
         </div>
     </section>
@@ -50,12 +50,57 @@
                 <div class="title-parcours">
                     <i class="fa-solid fa-graduation-cap"></i>
                     <h3>Parcours :</h3>
+                </div>           
+                <div class="formation">
+                    <i class="fa-regular fa-award"></i>
+                    <h4 class="etablissement">Lycée Tours Fondettes Agrocampus </h4>
+                    <p>Cursus de BAC général dans le lycée Tours Fondettes Agrocampus avec l'optention du baccalauréat général avec mention Assez Bien avec les spécialités Mathématiques, Biologie-Écologie et Physique-chimie</p>
                 </div>
-                <i class="fa-light fa-trophy-star" style="color: #B197FC;"></i>
-                <p>Je m'apelle <span class="presentation-important">Benjamin Lorieul</span>, jeune apprenti développeur web de 18ans. <br></p>
-                <p>J'adore coder des projets, construire des applications ou créer des algorithmes pour résoudre divers problèmes.</p>
-                <p>Je suis principalement axé sur la création de <span class="presentation-important">sites web</span> ainsi que de <span class="presentation-important">solutions logicielles et applications</span>.</p>
-                <p>J'ai appris majoritairement en auto-didacte avec <span class="presentation-important">HTML5, CSS et PHP</span> et je suis aujourd'hui étudiant en 1ère année de <span class="presentation-important">BTS SIO SLAM</span> dans la ville de Tours.</p>
+                <div class="formation">
+                    <i class="fa-solid fa-spinner"></i>
+                    <h4 class="etablissement">ISCB</h4>
+                    <p><span class="important">BTS SIO SLAM</span> à l'école ISCB - Je suis en alternance pour le <span class="important">ministère des armées </span> / <span class="important">DRHAT</span>. J'ai choisis l'option <span class="important">SLAM</span> afin de concrétiser le lancement de mon projet d'avenir, devenir un des meilleurs <span class="important">développeur web</span>. Tout en gagnant de l'expérience professionelle, je suis une formation de 2 ans qui va me permettre d'acquérir de nombreuses <span class="important">qualités professionelles et techniques</span>.</p>
+                </div>
+                <div class="formation">
+                    <i class="fa-sharp fa-solid fa-lightbulb-gear"></i>
+                    <h4 class="etablissement">Aspiration</h4>
+                    <p>Après l'obtention de mon BTS en 2026, je me dirigerais vers une <span class="important">licence pro</span> en developpemnt web puis sur un <span class="important">MASTER</span> afin d'obtenir un <span class="important">BAC+5</span>. Je souhaites continuer en <span class="important">alternance</span> afin de travailler sur des projets concrets qui m'aiderons à atteindre mes objectifs.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="section-competences">
+        <div class="container-competences">
+            <div class="title-competences">
+                <i class="fa-solid fa-code"></i>
+                <h3>Languages :</h3>
+            </div>
+            <div class="competences">
+                <div class="competence">
+                    <img src="img/Csharp_Logo.png" alt="C#">
+                </div>
+                <div class="competence">
+                    <img src="img/PHP_Logo.png" alt="PHP">
+                </div>
+                <div class="competence">
+                    <img src="img/Python_Logo.png" alt="Python">
+                </div>
+                <div class="competence">
+                    <img src="img/CSS3_Logo.png" alt="CSS3">
+                </div>
+                <div class="competence">
+                    <img src="img/HTML5_Logo.png" alt="HTML5">
+                </div>
+                <div class="competence">
+                    <img src="img/JavaScript_Logo.png" alt="JavaScript">
+                </div>
+                <div class="competence">
+                    <img src="img/MySql_Logo.png" alt="SQL">
+                </div>
+                <div class="competence">
+                    <img src="img/JavaScript_Logo.png" alt="JavaScript">
+                </div>
             </div>
         </div>
     </section>
